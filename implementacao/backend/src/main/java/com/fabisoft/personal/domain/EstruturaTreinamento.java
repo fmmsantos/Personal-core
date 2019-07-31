aa07839d4bf969352b002fc9e03aa382b2f3b0cf
fmmsantos <45079131+fmmsantos@users.noreply.github.com>
2019-07-31 17:03:48 -0300

    atualizacoes3

--- implementacao/backend/src/main/java/com/fabisoft/personal/domain/EstruturaTreinamento.java
+++ implementacao/backend/src/main/java/com/fabisoft/personal/domain/EstruturaTreinamento.java
@@ -1,7 +1,28 @@
 package com.fabisoft.personal.domain;
 
+import com.fabisoft.personal.enuns.Divisao;
+import com.fabisoft.personal.enuns.Metodo;
+import com.fabisoft.personal.enuns.Nivel;
+import com.fabisoft.personal.enuns.Sistema;
+import com.fabisoft.personal.enuns.Velocidade;
+
 public class EstruturaTreinamento {
 	private Long id;
-	private  Matricula matricula;
+	private  Aluno aluno;
+	private int numeroExerc;
+	private int numeroSerie;
+	private int repeticoes;
+	private int intervalo;
+	private int frequeSemanal;
+	private int serieTotal;
+	private int numeroTreino;
+	private int numerosemana;
+	private Divisao divisao;
+	private Metodo metodo;
+	private Nivel nivel;
+	private Sistema sistema;
+	private Velocidade velocidade;
+	
+	
 
 }
--- implementacao/backend/src/main/java/com/fabisoft/personal/domain/Matricula.java
+++ implementacao/backend/src/main/java/com/fabisoft/personal/domain/Matricula.java
@@ -1,6 +1,36 @@
 package com.fabisoft.personal.domain;
 
+import java.util.Date;
+
 public class Matricula {
 	private Long id;
+	private Aluno aluno;
+	private Date dataInicio;
+	private Date dataVencimento;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Aluno getAluno() {
+		return aluno;
+	}
+	public void setAluno(Aluno aluno) {
+		this.aluno = aluno;
+	}
+	public Date getDataInicio() {
+		return dataInicio;
+	}
+	public void setDataInicio(Date dataInicio) {
+		this.dataInicio = dataInicio;
+	}
+	public Date getDataVencimento() {
+		return dataVencimento;
+	}
+	public void setDataVencimento(Date dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
 
 }
